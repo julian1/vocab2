aadb3b46b735138d0d54a86699e6ba230a702f07
julian1 <julian.atkinson71@gmail.com>
2014-08-06 09:02:43 +1000

    update

--- sql/hhh.rb
+++ sql/hhh.rb
@@ -1,5 +1,5 @@
 
-# Example 
+# Example,
 # <skos:Concept rdf:about="http://vocab.nerc.ac.uk/collection/L06/current/31/">
 #   <skos:inScheme rdf:resource="http://environment.data.gov.au/aodn//def/Platform"/>
 #   <skos:prefLabel xml:lang="en">Research Vessel</skos:prefLabel>
@@ -22,9 +22,19 @@ def prepare_statements( conn)
 
   conn.prepare('all_terms', <<-EOS
     select v.vocabulary_term_name
+    ,v.vocabulary_term_uid
     from contr_vocab_db.vocabulary_term_table v
   EOS
   )
+
+
+#   conn.prepare('term', <<-EOS
+#     select *
+#     from contr_vocab_db.vocabulary_term_table v
+#     where v.vocabulary_term_name = $1
+#   EOS
+#   )
+
 
   conn.prepare('definition', <<-EOS
     select
@@ -54,11 +64,12 @@ def prepare_statements( conn)
   conn.prepare('narrower', <<-EOS
     -- narrower
     select
-
     v.vocabulary_term_uid,
     -- trim(trailing from o.vocabulary_term_uid) as narrower,
 
-    o.vocabulary_term_name as narrower
+    o.vocabulary_term_name as narrower -- this thing still needs to be mapped to the uri
+                                      -- not sure the db is correctly structured.
+
     from contr_vocab_db.vocabulary_term_table v
     left join contr_vocab_db.reference_source_table r on r.reference_id = v.reference_source_id
     left join contr_vocab_db.subject_term_table s on s.vocabulary_term_name = v.vocabulary_term_name
@@ -76,96 +87,65 @@ end
 prepare_statements( conn)
 
 
-# puts "all_terms" 
-# conn.exec_prepared('all_terms').each { |row| 
-#   puts row
-# }
-# 
-
-term = "L'Astrolabe"
-
-# puts "definition #{ conn.exec_prepared('definition', [term])[0] } "  
-# puts "source #{ conn.exec_prepared('source', [term])[0] } "  
-# puts "about #{ conn.exec_prepared('about', [term])[0] } "  
-# puts "narrower" 
-# conn.exec_prepared('narrower', [term]).each { |row| 
-#   puts row
-# } 
-# 
-
-
-## the narrower resource isn't correct. It should be a uri. 
-
-
-def dump_skos( conn, term)
-
-  puts <<-EOS
-  <skos:Concept rdf:about="#{ conn.exec_prepared('about', [term])[0]['about'] }">
-    <skos:prefLabel xml:lang="en">#{term}</skos:prefLabel>
-    <skos:definition>#{ conn.exec_prepared('definition', [term])[0]['definition']}</skos:definition>
-    <dc:source>#{ conn.exec_prepared('source', [term])[0]['source'] }</dc:source>
-
-    #{ s = ""
-      conn.exec_prepared('narrower', [term]).each { |row|
-        s += <<-EOS
-
-    <skos:narrower rdf:resource="#{row['narrower']}"/>
-        EOS
-      }
-      s }
-  </skos:Concept>
-
-  EOS
-
+def dump_all_terms(conn)
+  puts "dumping all_terms" 
+  conn.exec_prepared('all_terms').each { |row| 
+    puts row
+  }
 end
 
 
-dump_skos( conn, term)
+def dump_fields(conn, term)
+  puts "definition #{ conn.exec_prepared('definition', [term])[0] } "  
+  puts "source #{ conn.exec_prepared('source', [term])[0] } "  
+  puts "about #{ conn.exec_prepared('about', [term])[0] } "  
+  puts "narrower" 
+  conn.exec_prepared('narrower', [term]).each { |row| 
+    puts row
+  } 
+end
+
+
+def dump_skos(conn, term)
+  puts "dumping skos"
+  ## the narrower resource isn't correct. It should be a uri. 
+  puts <<-EOS
+    <skos:Concept rdf:about="#{ conn.exec_prepared('about', [term])[0]['about'] }">
+      <skos:prefLabel xml:lang="en">#{term}</skos:prefLabel>
+      <skos:definition>#{ conn.exec_prepared('definition', [term])[0]['definition']}</skos:definition>
+      <dc:source>#{ conn.exec_prepared('source', [term])[0]['source'] }</dc:source>
+
+      #{ s = ""
+        conn.exec_prepared('narrower', [term]).each { |row|
+          s += <<-EOS
+
+      <skos:narrower rdf:resource="#{row['narrower']}"/>
+          EOS
+        }
+        s }
+    </skos:Concept>
+  EOS
+end
+
+
+# dump_all_terms( conn)
+term = "L'Astrolabe"
+
+dump_fields(conn, term)
+# dump_skos( conn, term)
+
+
 
 abort( 'finished' )
 
-# puts get_citation( conn, "research vessel" ) 
-# # abort( 'finished')
+
+
+
+
+
+ 
 # 
-# 
-# 
-# 
-# 
-# 
-# #   res.each { |row|
-# #       puts row #
-# #       #puts row['vocabulary_term_name']
-# #       puts <<-EOS
-# #       <skos:Concept rdf:about= #{row['about']} >
-# #         <skos:prefLabel xml:lang="en">#{row['prefLabel']}</skos:prefLabel>
-# #         <skos:definition>#{row['definition']}</skos:definition>
-# #         <dc:source>#{row['source']}</dc:source>
-# # 
-# #   EOS
-# #  }
-# end
-# 
-# 
-# func1( conn, "dummy" );
-# 
-# 
-# def get_narrower( conn, x)
-# 
-#   conn.prepare('statement1', <<-EOS
-#     -- narrower
-#     select
-#     v.vocabulary_term_name,
-#     o.vocabulary_term_name as child
-#     from contr_vocab_db.vocabulary_term_table v
-#     left join contr_vocab_db.reference_source_table r on r.reference_id = v.reference_source_id
-#     left join contr_vocab_db.subject_term_table s on s.vocabulary_term_name = v.vocabulary_term_name
-#     left join contr_vocab_db.internal_associated_terms_table a on a.subject_term_id = s.subject_term_id
-#     left join contr_vocab_db.object_term_table o on a.object_term_id = o.object_term_id
-#     where a.association_type_name = 'isInstanceOf'
-#     and v.vocabulary_term_name = $1
-#   EOS
-#   )
-# 
+#
 #   # we should be encoding ruby tuples  according to the name
 #   # might have multiple results
 #   
