c4529ee6e96994c38975f5289a3ab8cff507195f
julian1 <julian.atkinson71@gmail.com>
2014-08-05 17:14:31 +1000

    update

--- sql/hhh.rb
+++ sql/hhh.rb
@@ -1,5 +1,5 @@
 
-# <skos:Concept rdf:about="http://vocab.nerc.ac.uk/collection/L06/current/31/"> 
+# <skos:Concept rdf:about="http://vocab.nerc.ac.uk/collection/L06/current/31/">
 #   <skos:inScheme rdf:resource="http://environment.data.gov.au/aodn//def/Platform"/>
 #   <skos:prefLabel xml:lang="en">Research Vessel</skos:prefLabel>
 #   <skos:definition>A platform of any size operating on the surface of the water column in unpredictable locations that is specifically equipped manned and operated for scientific purposes, usually oceanographic research.</skos:definition>
@@ -9,7 +9,7 @@
 #   <skos:narrower rdf:resource="http://vocab.aodn.org.au/aodn/def/Platform/Instance/65"/>
 #   <skos:narrower rdf:resource="http://vocab.aodn.org.au/aodn/def/Platform/Instance/71"/>
 # </skos:Concept>
-# 
+#
 
 
 require 'pg'
@@ -31,27 +31,27 @@ conn = PG::Connection.open(:host => "localhost", :dbname => "vocab", :user => "c
 # res = conn.exec( <<-EOS
 #   -- concept
 #   create temporary view concept as
-#   select 
+#   select
 #     trim(trailing from v.vocabulary_term_uid) as about,
 #     v.vocabulary_term_name as prefLabel,
 #     v.vocabulary_term_definition as definition,
 #     r.citation_string as source
-#   from contr_vocab_db.vocabulary_term_table v 
-#   left join contr_vocab_db.reference_source_table r on r.reference_id = v.reference_source_id 
+#   from contr_vocab_db.vocabulary_term_table v
+#   left join contr_vocab_db.reference_source_table r on r.reference_id = v.reference_source_id
 #   ;
-# 
-#   select about from concept ; 
+#
+#   select about from concept ;
 # EOS
 # )
-# 
+#
 # res.each { |row|
-#     puts row # 
-# } 
-# 
+#     puts row #
+# }
+#
 # abort( 'finish' )
 
 # how should we piece this stuff together...
-# pass in connection and term??? 
+# pass in connection and term???
 
 
 def func1( conn, x)
@@ -60,70 +60,62 @@ def func1( conn, x)
     -- concept
 
     create temporary view concept as
-    select 
+    select
       trim(trailing from v.vocabulary_term_uid) as about,
       v.vocabulary_term_name as prefLabel,
       v.vocabulary_term_definition as definition,
       r.citation_string as source
-    from contr_vocab_db.vocabulary_term_table v 
-    left join contr_vocab_db.reference_source_table r on r.reference_id = v.reference_source_id 
+    from contr_vocab_db.vocabulary_term_table v
+    left join contr_vocab_db.reference_source_table r on r.reference_id = v.reference_source_id
     ;
 
-    select * from concept where preflabel = 'research vessel'; 
+    select * from concept where preflabel = 'research vessel';
   EOS
   )
 
   res.each { |row|
-      puts row # 
+      puts row #
       #puts row['vocabulary_term_name']
       puts <<-EOS
-      <skos:Concept rdf:about= #{row['about']} >  
+      <skos:Concept rdf:about= #{row['about']} >
         <skos:prefLabel xml:lang="en">#{row['prefLabel']}</skos:prefLabel>
         <skos:definition>#{row['definition']}</skos:definition>
         <dc:source>#{row['source']}</dc:source>
-      
+
   EOS
-    } 
+    }
 end
 
 
 
-def func2( conn, x)
-
-
-# conn.prepare('statement1', 'select $1::Int as result')
-# conn.exec_prepared('statement1', [ 123 ] ) .each { |row|
-#     puts row # 
-# } 
-# #
+def get_narrower( conn, x)
 
   conn.prepare('statement1', <<-EOS
-
     -- narrower
-
-    -- create temporary view narrower as
     select
-      v.vocabulary_term_name, 
-      o.vocabulary_term_name as child
-    from contr_vocab_db.vocabulary_term_table v 
-    left join contr_vocab_db.reference_source_table r on r.reference_id = v.reference_source_id 
+    v.vocabulary_term_name,
+    o.vocabulary_term_name as child
+    from contr_vocab_db.vocabulary_term_table v
+    left join contr_vocab_db.reference_source_table r on r.reference_id = v.reference_source_id
     left join contr_vocab_db.subject_term_table s on s.vocabulary_term_name = v.vocabulary_term_name
-    left join contr_vocab_db.internal_associated_terms_table a on a.subject_term_id = s.subject_term_id 
+    left join contr_vocab_db.internal_associated_terms_table a on a.subject_term_id = s.subject_term_id
     left join contr_vocab_db.object_term_table o on a.object_term_id = o.object_term_id
     where a.association_type_name = 'isInstanceOf'
-  and v.vocabulary_term_name = $1
-        ; 
-  --  select * from narrower --   
+    and v.vocabulary_term_name = $1
   EOS
   )
 
+  # we should be encoding ruby tuples  according to the name
+
+  # might have multiple results
 
   conn.exec_prepared('statement1', [ x ] ) .each { |row|
-      puts row # 
-  } 
-  abort( 'abort')
+      puts row #
+  }
 end
 
+get_narrower( conn, "L'Astrolabe" )
 
-func2( conn, "L'Astrolabe" )
+
+
 
