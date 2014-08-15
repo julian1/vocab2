60d7244cb1b660be879e39e409ce02d3fc03918a
julian1 <julian.atkinson71@gmail.com>
2014-08-15 14:05:51 +1000

    update

--- latex/build_report.sh
+++ latex/build_report.sh
@@ -4,6 +4,8 @@ INPUT=report.tex
 TMP=./tmp
 #DIR=./latex 
 
+test -d $TMP || mkdir $TMP
+
 
 rm report.pdf 2> /dev/null; 
 pdflatex -halt-on-error -output-directory $TMP $INPUT && evince $TMP/report.pdf 
