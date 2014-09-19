186a32ffcc42e33215c36ea2a9d2191356e8241a
julian1 <julian.atkinson71@gmail.com>
2014-09-19 14:21:54 +1000

    Stuff for liquibase

---
+++ run_liquibase.sh
@@ -0,0 +1,9 @@
+
+java -jar liquibase.jar update
+
+#      --changeLogFile=db.changelog.xml \
+#      --username=scott \
+#      --password=tiger \
+#      --url="jdbc:oracle:thin:@localhost:1521:oracle" \
+#      --classpath=/usr/share/java/postgresql-jdbc3-9.2.jar  \
+#      --driver=oracle.jdbc.OracleDriver \
