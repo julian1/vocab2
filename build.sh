9d5fe4304c8e88e961a2f5c1a208cf205ba3b40d
julian1 <julian.atkinson71@gmail.com>
2014-09-19 16:05:25 +1000

    change build scripts, so we don't have to disconnect

--- build.sh
+++ build.sh
@@ -1,12 +1,25 @@
 
-# make sure we have the necessary user
-if false; then
+# create the db - only do once - to avoid having to disconnect 
+if false ; then
 	sudo -u postgres psql -c 'drop database if exists vocab' || exit
 	sudo -u postgres psql -c 'drop user if exists contr_vocab_db' || exit
 	sudo -u postgres psql -c 'create user contr_vocab_db with password $$contr_vocab_db$$ createdb' || exit
 	echo '127.0.0.1:5432:*:contr_vocab_db:contr_vocab_db' > ~/.pgpass 
+
+	# create db
+	psql -h 127.0.0.1 -U contr_vocab_db -d postgres -c 'create database vocab' || exit
+	
+
+	# import into public schema
+	psql -h 127.0.0.1 -U contr_vocab_db -d vocab -f misc/admin93.sql
+
+	echo done 
+
 fi
 
 # connect as correct user, since this is a property of the session which cannot be changed
-psql -h 127.0.0.1 -U contr_vocab_db -d postgres -f build.sql
+psql -h 127.0.0.1 -U contr_vocab_db -d vocab -f build.sql
+
+
+# psql -h 127.0.0.1 -U contr_vocab_db -d vocab -c 'select 1;' 
 
--- build.sql
+++ build.sql
@@ -1,26 +1,22 @@
 
--- Usage, psql -h 127.0.0.1 -U meteo -f build.sql  2>&1 | tee log.txt
 
 -- Usage psql -h 127.0.0.1 -U meteo -f build.sql
 
--- role changes must be done as postgres
--- sudo -u postgres psql -c 'create user contr_vocab_db password $$contr_vocab_db$$' 
--- sudo -u postgres psql -c 'grant contr_vocab_db to meteo '
+
+-- we don't want to do the dropping and creation of the database usually.
+-- because then we get logged out.
 
 
-drop database if exists vocab; 
-create database vocab; 
 
-\c vocab;
+-- select admin.drop_objects_in_schema( 'contr_vocab_db' );
 
+drop schema if exists contr_vocab_db cascade; 
 create schema contr_vocab_db ; 
 
--- alter schema contr_vocab_db owner to contr_vocab_db ; 
--- set search_path = contr_vocab_db, public;
+
 
 \i docs_seb/Vocab_db_table_creation_10092014.sql; 
 
 \i docs_seb/test_seb_vocab_v5.txt; 
 
-\i misc/admin93.sql
 
