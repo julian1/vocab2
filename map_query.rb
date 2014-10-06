76c9ae1629ee3f356f4fae37c4e2cde1ace43c03
julian1 <julian.atkinson71@gmail.com>
2014-10-07 10:55:47 +1100

    update map_query

--- map_query.rb
+++ map_query.rb
@@ -5,6 +5,7 @@
 # take the code, to the data.
 # this would be better as a fold, from which we can create a map if we want. 
 
+# actually would be better if it was a fold operation - it's not as clean as a real fold
 
 require 'erb'
 require 'pg'
@@ -29,9 +30,27 @@ def map_query( conn, query, args, &code )
 end
 
 
-j = map_query( conn, 'select * from _rdf where predicate = $$rdf:type$$ and object = $1 ', ['skos:ConceptScheme']) do |x|
-	x['subject']
+def fold_query( conn, query, args, xs, &code )
+
+	conn.exec( query, args ).each do |row|
+		# puts "-> #{row['subject']}"
+		code.call( row, xs ) 	
+	end
+	xs
+end
+
+
+
+# 
+# j = map_query( conn, 'select * from _rdf where predicate = $$rdf:type$$ and object = $1 ', ['skos:ConceptScheme']) do |x|
+# 	x['subject']
+# end 
+# 
+
+j = fold_query( conn, 'select * from _rdf where predicate = $$rdf:type$$ and object = $1 ', ['skos:ConceptScheme'], []) do |row, xs|
+	xs << row['subject']
 end 
+
 
 
 puts j
