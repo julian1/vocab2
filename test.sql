bfd552e4409bbc8b3a4018d7a12aa0be29c0859c
julian1 <julian.atkinson71@gmail.com>
2014-10-09 11:18:25 +1100

    update

--- test.sql
+++ test.sql
@@ -8,10 +8,9 @@ select
 	csc2.name as parent_object
 
 from vocabulary_term vt
-left join term_category_classification tcc on tcc.vocabulary_term_id = vt.id
-left join classification_scheme_category csc on tcc.classification_scheme_category_id = csc.id
-
-
-
-left join classification_scheme_association csa on csa.classification_scheme_category_id = csc.id
-left join classification_scheme_category csc2 on csa.parent_classification_scheme_category_id = csc2.id
+-- get second-tier category
+join term_category_classification tcc on tcc.vocabulary_term_id = vt.id
+join classification_scheme_category csc on tcc.classification_scheme_category_id = csc.id
+-- get first-tier category
+join classification_scheme_association csa on csa.classification_scheme_category_id = csc.id
+join classification_scheme_category csc2 on csa.parent_classification_scheme_category_id = csc2.id
