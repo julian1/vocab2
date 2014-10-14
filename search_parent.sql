c7f33053971cb2627672d862006be998e450480c
julian1 <julian.atkinson71@gmail.com>
2014-10-14 18:13:29 +1100

    update

--- search_parent.sql
+++ search_parent.sql
@@ -11,22 +11,30 @@ left join internal_associated_terms  iat on iat.subject_vocabulary_term_id = vt.
 left join vocabulary_term iat_vt on iat.object_vocabulary_term_id = iat_vt.id  and iat.association_type_name = 'isInstanceOf' 
 ;
 
-
+-- drop table fuck_viewx; 
+-- create table fuck_viewx (parent varchar, child varchar) ;
+-- insert into fuck_viewx( parent, child) values (('a', 'b' ) ); 
 
 
 
 WITH RECURSIVE t( child, parent ) AS (
 
-	select  v.child, v.parent from fuck_view v where v.child = child
+	-- with an initial condition. it assumes that there's always something further to go ? 
+
+	-- terminating condition . except it's not ?  it's the first condition.  
+	select  child, child from fuck_view v where v.child = child
 
 	union all
 
-	select child, parent from t where t.parent = child 
+	-- but is this actually self-referential here ? 
+	select v2.child, v2.parent 
+		from fuck_view v1 left join fuck_view v2 on v2.parent = v1.child  where v2.parent is not null
 
 	--  this doesn't seem to be used.	
 )
 
-	select  parent from t where child = 'http://vocab.aodn.org.au/def/platform/271'; 
+	-- select  * from t where child = 'http://vocab.aodn.org.au/def/platform/271'; 
+	select  * from t where child = 'http://vocab.nerc.ac.uk/collection/L06/current/32'; 
 
 
 
