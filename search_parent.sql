c5fc4be5071013a79a087a8648d52ebb48795244
julian1 <julian.atkinson71@gmail.com>
2014-10-15 10:54:01 +1100

    kind of working

--- search_parent.sql
+++ search_parent.sql
@@ -2,44 +2,47 @@
 
 create or replace view helper_view as
 
-select 
-	vt.uid as child, 
+select
+	vt.uid as child,
 	iat_vt.uid as parent
-from vocabulary_term vt 
--- left join term_category_classification tcc on tcc.vocabulary_term_id = vt.id 
-left join internal_associated_terms  iat on iat.subject_vocabulary_term_id = vt.id 
-left join vocabulary_term iat_vt on iat.object_vocabulary_term_id = iat_vt.id  and iat.association_type_name = 'isInstanceOf' 
+from vocabulary_term vt
+-- left join term_category_classification tcc on tcc.vocabulary_term_id = vt.id
+left join internal_associated_terms  iat on iat.subject_vocabulary_term_id = vt.id
+left join vocabulary_term iat_vt on iat.object_vocabulary_term_id = iat_vt.id  and iat.association_type_name = 'isInstanceOf'
 ;
 
 
 -- Strategy is to return all child, parent relationships via the recursion .
 -- then filter for where there's no parent.
 
+-- the issue is that the initial starting point has to be be placed in the clause...
+
 WITH RECURSIVE t( child, parent ) AS (
 
-	-- add the v	
-	select h.child, h.child from helper_view h where h.child = 'http://vocab.aodn.org.au/def/platform/271'
+	-- add the v
+	select h.child, h.parent
+		from helper_view h
+		where h.child = 'http://vocab.aodn.org.au/def/platform/271'
 
 	union all
 
-	-- we don't want to avoid the sutjj 
-	select h.child, h.parent  
-		from helper_view h join t on (h.child = t.parent )
-
-
+	-- this looks correct
+	select h.child, h.parent
+		from helper_view h
+		join t on (h.child = t.parent )
 )
 
-	select  * from t --where child = 'http://vocab.aodn.org.au/def/platform/271' -- and parent is not null  
+	select  * from t --where child = 'http://vocab.aodn.org.au/def/platform/271' -- and parent is not null
 
 
 
 
-	-- but is this actually self-referential here ? 
+	-- but is this actually self-referential here ?
 	-- select child, parent from t where parent is not null
-	--  this doesn't seem to be used.	
+	--  this doesn't seem to be used.
 
 		-- from helper_view fv join t on (fv.parent = t.child  )
-		-- this won't actually return anything if there's no parent ... but it doesn't mean we've found the root 
+		-- this won't actually return anything if there's no parent ... but it doesn't mean we've found the root
 		-- from helper_view fv join t on (fv.child = t.parent )
 
 
@@ -52,35 +55,35 @@ WITH RECURSIVE t( child, parent ) AS (
 	-- we s
 
 
--- drop table helper_viewx; 
+-- drop table helper_viewx;
 -- create table helper_viewx (parent varchar, child varchar) ;
--- insert into helper_viewx( parent, child) values (('a', 'b' ) ); 
+-- insert into helper_viewx( parent, child) values (('a', 'b' ) );
 
 -- *****
--- find everything that exists in the tree above where we are. 
--- then perform a filter task on that for just those nodes that don't have parents. 
+-- find everything that exists in the tree above where we are.
+-- then perform a filter task on that for just those nodes that don't have parents.
 
--- should the thing be recursive? 
+-- should the thing be recursive?
 
--- IMPORTANT Note, we should 
+-- IMPORTANT Note, we should
 
 -- this would be easier to write as a series of joins and if else statements.  to test all the values that are at the root.
 
 -- not sure - it ought to be possible to do the first bit with the function easily.
 
-	-- with an initial condition. it assumes that there's always something further to go ? 
+	-- with an initial condition. it assumes that there's always something further to go ?
 
-	-- this is not a terminating condition . it's an initial condition.  
-	-- just add the intial condition - which is needed if we call this on a top node 
-
-
-
---	 select  * from t where child = 'http://vocab.nerc.ac.uk/collection/L06/current/32'; 
+	-- this is not a terminating condition . it's an initial condition.
+	-- just add the intial condition - which is needed if we call this on a top node
 
 
 
+--	 select  * from t where child = 'http://vocab.nerc.ac.uk/collection/L06/current/32';
 
---  this doesn't seem to be used.	
+
+
+
+--  this doesn't seem to be used.
 
 
 
