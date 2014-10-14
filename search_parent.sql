0a319bbe5d775d38f3a4b515db295002769dc622
julian1 <julian.atkinson71@gmail.com>
2014-10-14 17:16:35 +1100

    update

--- search_parent.sql
+++ search_parent.sql
@@ -1,5 +1,16 @@
 
 
+create or replace fuck_view as
+
+select 
+	vt.uid as child, 
+	iat_vt.uid as parent
+from vocabulary_term vt 
+left join term_category_classification tcc on tcc.vocabulary_term_id = vt.id 
+left join internal_associated_terms  iat on iat.subject_vocabulary_term_id = vt.id 
+left join vocabulary_term iat_vt on iat.object_vocabulary_term_id = iat_vt.id  and iat.association_type_name = 'isInstanceOf' 
+;
+
 
 
 
@@ -7,15 +18,20 @@
 WITH RECURSIVE t( fuck ) AS (
 
 	select  1 as fuck
+
 	union all
 
-	select fuck + 1 from t where fuck < 100 
+	select fuck + 1 from t where fuck < 10 
 
 	--  this doesn't seem to be used.	
 )
 
 	select  fuck from t; 
 
+
+
+
+--  this doesn't seem to be used.	
 
 
 
