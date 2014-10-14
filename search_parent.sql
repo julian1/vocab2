9b61c59c75b28764a5746dbe893982fefd5a96d1
julian1 <julian.atkinson71@gmail.com>
2014-10-14 17:10:46 +1100

    update

--- search_parent.sql
+++ search_parent.sql
@@ -1,27 +1,21 @@
 
 
-WITH RECURSIVE t(n) AS (
-    VALUES (1)
-  UNION ALL
-    SELECT n+1 
-	FROM t WHERE n < 100
+
+
+
+
+WITH RECURSIVE t( fuck ) AS (
+
+	select  1 as fuck
+	union all
+
+	select fuck + 1 from t where fuck < 100 
+
+	--  this doesn't seem to be used.	
 )
-SELECT sum(n) FROM t;
 
+	select  fuck from t; 
 
-
-select 
-	trim( vt.uid) as uid, 
-	trim( iat_vt.uid) as iat_vt 
-
---  this doesn't seem to be used.	
-from vocabulary_term vt 
-left join term_category_classification tcc on tcc.vocabulary_term_id = vt.id 
-left join internal_associated_terms  iat on iat.subject_vocabulary_term_id = vt.id 
-left join vocabulary_term iat_vt on iat.object_vocabulary_term_id = iat_vt.id  
-and iat.association_type_name = 'isInstanceOf' 
-
-;
 
 
 
