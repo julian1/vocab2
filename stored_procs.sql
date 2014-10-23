f9ca45a350507c55dc3b27440792a1871d067bf9
julian1 <julian.atkinson71@gmail.com>
2014-10-23 11:00:52 +1100

    update stored procs

--- stored_procs.sql
+++ stored_procs.sql
@@ -1,25 +1,6 @@
 
--- ok, so what do we do about the organisation_id ?
--- and the id.
 
--- cqrs - we have denormalized views.
--- the list is the and we're going to need a list.
-
-
--- I don't think we can actually denormalize it. yes we can, we just go back to the prior table
--- and use the obvious unique non-id value that's there as the primary field.
-
--- eg. for vocabulary_term we use the online_reference_resource rather than that tables id.
-
---  id                        | integer                | not null default nextval('reference_source_id_seq'::regclass)
---  citation_string           | character varying(110) |
--- online_reference_resource | character varying(200) |
---  organisation_id           | integer                |
--- online_term_resource      | character varying(110) |
-
-
-drop function reference_source_list() ;
-
+-- we need to put a unique constraint on the online_reference_resource
 
 create or replace function reference_source_list()
 returns table ( online_reference_resource varchar, citation_string varchar, organisation_acronym varchar, online_term_resource varchar, id integer)
@@ -44,26 +25,6 @@ return query
 end
 $$ language plpgsql;
 
-
--- select id from  reference_source_select
--- should perhaps return the id of the resource that we deleted,
-
--- might be easier to express this with 'using reference_source_select( xxx) rfs ' 
--- but we need to create a dummy entry ... so we can test the delete 
-
--- this should generate an error if the thing doesn't exist.
-
-
--- id                        | integer                | not null default nextval('reference_source_id_seq'::regclass)
--- citation_string           | character varying(110) | 
--- online_reference_resource | character varying(200) | 
--- organisation_id           | integer                | 
--- online_term_resource      | character varying(110) | 
-
--- we are going to have to construct select from parameters and then feed that in...
--- should be using a function to get the organisation id.
---  select online_reference_resource, citation_string, organisation_acronym_ , online_term_resource 
---  with organisation as (select id from organisation o where o.acronym = organisation_acronym_ ) 
 
 create or replace function reference_source_insert( online_reference_resource varchar, citation_string varchar, organisation_acronym varchar, online_term_resource varchar )
 returns void 
@@ -96,10 +57,5 @@ begin
   end if; 
 end
 $$ language plpgsql;
-
-
-
-
-
 
 
