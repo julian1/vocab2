9c87665bc2c9f4fe3717f43d63582d5c295a9ab6
julian1 <julian.atkinson71@gmail.com>
2014-10-23 11:00:24 +1100

    update build script to also run migrations

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@
 # connect as correct user, since this is a property of the session which cannot be changed
 psql -h 127.0.0.1 -U contr_vocab_db -d vocab -f build.sql > /dev/null
 
+# only if using admin93.
+# psql -h 127.0.0.1 -U contr_vocab_db -d vocab -f ~/imos/projects/db/admin93.sql   
+
+pushd migration/; ./run_migrations.sh; popd
 
 
 
