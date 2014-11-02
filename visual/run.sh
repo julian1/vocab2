4515e53ea3c9ba683108499472bc6c91b0229c5c
julian1 <julian.atkinson71@gmail.com>
2014-11-03 08:13:56 +1100

    visual schema to use directory output rather than tmp

--- visual/run.sh
+++ visual/run.sh
@@ -1,7 +1,7 @@
 
-output=./tmp
+output=./output
 rm -rf $output
 mkdir $output
 
-java -jar schemaSpy_5.0.0.jar -t my.properties -s contr_vocab_db -u contr_vocab_db -p contr_vocab_db -o tmp/
+java -jar schemaSpy_5.0.0.jar -t my.properties -s contr_vocab_db -u contr_vocab_db -p contr_vocab_db -o $output
 
