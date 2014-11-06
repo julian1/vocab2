3cddfd5003d2b5766c6d5129fb80ab0863224c1e
julian1 <julian.atkinson71@gmail.com>
2014-11-06 17:11:15 +1100

    update

--- grails/test3.groovy
+++ grails/test3.groovy
@@ -2,15 +2,22 @@
 package vocab;
 
 // Organisation
+o = new Organisation( name: 'csiro')
+o.notes = 'great organisation'
+o.save( flush: true, failOnError:true )
+
+o = Organisation.findWhere( name: 'csiro' )
+assert o != null
+o.delete( flush: true)
+
+
+
 
 // Person
-
 p = new Person( name: 'johnny' )
 p.save( flush: true, failOnError:true )
 
 p = Person.findWhere( name: 'johnny' )
 assert p != null
-
-p.delete()
-p.save( flush: true, failOnError:true )
+p.delete(flush: true)
 
